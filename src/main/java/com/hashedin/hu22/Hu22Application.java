e86d55864a5c521f6306647b0bae4e8099850a83
ritiksrivastava-delloite <ritsrivastav@deloitte.com>
2022-04-06 15:42:50 +0530

    Created The Project

--- src/main/java/com/hashedin/hu22/Hu22Application.java
+++ src/main/java/com/hashedin/hu22/Hu22Application.java
@@ -8,7 +8,11 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 public class Hu22Application extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(Hu22Application.class, args);
+
+		System.out.println("Hello");
+
 	}
 
 }
